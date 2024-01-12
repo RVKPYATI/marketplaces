c9e3563c480796dfa7222333596cbbe425d84328
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-12 16:29:51 +0300

    add check

--- src/app/components/WeeksDay/WeeksDay.jsx
+++ src/app/components/WeeksDay/WeeksDay.jsx
@@ -50,7 +50,7 @@ export const WeeksDay = () => {
                       <input
                         type="checkbox"
                         defaultValue={0}
-                        className="w-[17px] h-[17px] text-orange-400  bg-white border-[#C3C3C3] border-2 rounded-[2px] focus:ring-gray-300 focus:ring-2 cursor-pointer"
+                        className="w-[17px] h-[17px] text-[#F9CD46] bg-white border-[#bfbfbf] border-2 rounded-[2px] focus:ring-gray-300 focus:ring-0 cursor-pointer relative checked:after:absolute checked:after:bg-[url('/check.svg')] checked:after:top-0 checked:after:w-full checked:after:h-full"
                       />
                     </div>
                   </div>
