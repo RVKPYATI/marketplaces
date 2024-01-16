903b79473e5aba5d76970415ecdc9affefdc5703
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-16 09:30:31 +0300

    resize input fbs

--- src/app/components/Table/TableWildberris.jsx
+++ src/app/components/Table/TableWildberris.jsx
@@ -40,13 +40,13 @@ export const TableWildberris = ({ headers, products }) => {
                 </div>
               </th>
               <th align="center">
-                <div className="w-14 md:w-20 xl:w-[100px] p-2">
-                  <InputText style="bg-white border-2 border-gray-200" />
+                <div className="w-14 md:w-20 xl:w-[80px] p-2">
+                  <InputText style="bg-white border-2 border-gray-200 focus:border-none hover:bg-white" />
                 </div>
               </th>
               <th align="center">
                 <div className="w-14 md:w-20 xl:w-[100px] p-2">
-                  <InputText style="bg-white border-2 border-gray-200" />
+                  <InputText style="bg-white border-2 border-gray-200 focus:border-none hover:bg-white" />
                 </div>
               </th>
               <th align="center">
@@ -64,7 +64,7 @@ export const TableWildberris = ({ headers, products }) => {
               </th>
               <th align="center">
                 <div className="w-16 md:w-20 xl:w-[100px] p-2 flex items-center gap-1">
-                  <InputText style={"border-none"} />
+                  <InputText style=" border-none " />
                   <div className="text-[#8F8F8F] text-sm flex items-center">
                     ₽
                   </div>
