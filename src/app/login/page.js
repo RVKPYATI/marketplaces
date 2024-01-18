37d3a87d9868757348390e0bac903d22dd73d849
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-18 11:41:01 +0300

    edit login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import "../globals.css";
 
 export default function Login() {
   return (
-    <section className="bg-gray-50 bg-[url(/bg-login.jpg)] h-screen">
-      <div className="flex flex-col items-center justify-end px-6 py-8 mx-auto md:h-screen lg:py-0 ">
-        <div className="w-full h-screen bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+    <section className="bg-custom h-screen">
+      <div className="flex flex-col items-center justify-end px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full h-[90%] bg-white rounded-2xl shadow md:mt-0 sm:max-w-md xl:p-0 pt-7">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8 ">
             <div className="flex justify-center pb-8">
               <Image src="/main_logo.jpg" width={64} height={64} alt="logo" />
             </div>
@@ -15,7 +16,7 @@ export default function Login() {
                   type="email"
                   name="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#F9CD46] focus:border-[#F9CD46] block w-full p-3 placeholder:text-2xl placeholder:text-gray-400"
+                  className="bg-white mb-10 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#F9CD46] focus:border-[#F9CD46] block w-full px-3 py-4 placeholder:text-2xl placeholder:text-gray-300"
                   placeholder="Логин"
                   required=""
                 />
@@ -26,7 +27,7 @@ export default function Login() {
                   name="password"
                   id="password"
                   placeholder="Пароль"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#F9CD46] focus:border-[#F9CD46] block w-full p-3 placeholder:text-2xl placeholder:text-gray-400"
+                  className="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#F9CD46] focus:border-[#F9CD46] block w-full px-3 py-4 placeholder:text-2xl placeholder:text-gray-300"
                   required=""
                 />
               </div>
@@ -34,7 +35,7 @@ export default function Login() {
               <div className="pt-9">
                 <button
                   type="submit"
-                  className="w-full text-black font-semibold bg-[#ffcc09] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg px-5 py-2.5 text-center "
+                  className="w-full text-black font-bold bg-[#ffcc09] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg px-5 py-2.5 text-center "
                 >
                   Войти
                 </button>
