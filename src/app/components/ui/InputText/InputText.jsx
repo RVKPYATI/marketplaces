93f4512034a8cefc0cd90aa3e854caeb6d199e48
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-21 09:23:20 +0300

    edit login page

--- src/app/components/ui/InputText/InputText.jsx
+++ src/app/components/ui/InputText/InputText.jsx
@@ -3,7 +3,7 @@ export const InputText = ({ value, defaultValue = 0, style }) => {
   return (
     <input
       type="text"
-      className={`${style} ${yandexRegular.className} w-full text-[#000000] font-semibold text-center text-xs md:text-[15px] rounded-md bg-gray-200 hover:bg-gray-300 h-9 focus:ring-2 focus:ring-orange-300 focus:bg-white p-0`}
+      className={`${style} ${yandexRegular.className} w-full text-[#000000] font-semibold text-center text-xs md:text-[15px] rounded-md bg-[#ECEDF1] hover:bg-gray-300 h-9 focus:ring-2 focus:ring-orange-300 focus:bg-white p-0`}
       defaultValue={defaultValue}
       value={value}
     />
