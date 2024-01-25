4f58296b092985c56d47fff6bc52a2f1da3a7ca0
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-25 21:06:01 +0300

    remove text nextconfig

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
-"use client";
 import { MainBlock } from "./components/MainBlock/MainBlock";
-import { useRouter } from "next/navigation";
+
 export default function Home() {
-  const router = useRouter();
-  router.push("/wildberries");
   return <MainBlock />;
 }
