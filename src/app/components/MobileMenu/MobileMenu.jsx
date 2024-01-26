ae6ed5cea385ff076252a7b041653b5706b8f55d
Vasiliy Rusanov <rvk1986@mail.ru>
2024-01-26 11:39:08 +0300

    edit 26-01-2024 11-35

--- src/app/components/MobileMenu/MobileMenu.jsx
+++ src/app/components/MobileMenu/MobileMenu.jsx
@@ -79,13 +79,31 @@ export const MobileMenu = () => {
           </svg>
         ) : (
           <div className="w-10 ml-4 mt-4">
-            <Image
+            {/* <Image
               src="/close.svg"
               width={24}
               height={24}
               alt="close"
               className=""
-            />
+            /> */}
+            <svg
+              fill="#FFFFFF"
+              height="24px"
+              width="24px"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <g>
+                  <polygon
+                    points="512,59.076 452.922,0 256,196.922 59.076,0 0,59.076 196.922,256 0,452.922 59.076,512 256,315.076 452.922,512 
+			512,452.922 315.076,256 		"
+                  />
+                </g>
+              </g>
+            </svg>
           </div>
         )}
       </button>
